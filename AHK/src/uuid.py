3ea54ed1c5ddec6a4664666c27968bdfb183d611
Aaron Yau <mgsweet@126.com>
2021-12-26 17:02:38 +0800

    Update uuid.py

--- AHK/src/uuid.py
+++ AHK/src/uuid.py
@@ -1,12 +1,10 @@
 import uuid
 
-with open ('input.txt', 'r' ) as f:
+with open ('apexmaster.ahk') as f:
     content = f.read()
 
     new_uuid_str = 'global UUID = "' + uuid.uuid4().hex + '"'
     
-    content_new = re.sub(r'^global UUID = "\s+"$', content, new_uuid_str)
-
-    print(content_new)
+    content_new = re.sub(r'^global UUID = "\s+"$', new_uuid_str, content)
 
     f.write(content_new)
